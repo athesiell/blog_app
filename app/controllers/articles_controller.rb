f653482a1be66fc688db31f22c92d4314b3d2208
Aidar <ar.ibraev98@gmail.com>
2024-01-25 20:58:53 +0600

    Add from builder and methods

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -6,4 +6,24 @@ class ArticlesController < ApplicationController
   def show
     @articles = Article.find(params[:id])
   end
+
+  def new
+    @articles = Article.new
+  end
+
+  def create
+    @articles = Article.new(article_params)
+
+    if @articles.save
+      redirect_to @articles
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def article_params
+    params.require(:article).permit(:title, :body)
+  end
 end
